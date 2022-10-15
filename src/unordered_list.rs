508ebcc66baf30cabd4920ee908922bfa380a000
abhilekh <abhilekhgautam119@gmail.com>
2022-10-15 17:34:03 +0545

    use iterator instead of loop

--- src/unordered_list.rs
+++ src/unordered_list.rs
@@ -1,25 +1,20 @@
 
 /// unordered_list is the html's equivalent of ul tag
-pub struct unordered_list {
+pub struct UnorderedList {
     list_items: Vec<String>,
 }
 
 /// this function generates equivalent html code for a given unordered_list
 /// this returns html code as an String
-fn generate_unordered_list(list: unordered_list) -> String {
-    let mut ul_html = String::from("<ul>");
+fn generate_unordered_list(UnorderedList{list_items}: UnorderedList) -> String {
+   let list_item = list_items.into_iter()
+       .map(|item|{
 
-    for items in list.list_items {
-        let mut list_item = String::from("<li>");
-        list_item.push_str(items.as_str());
-        list_item.push_str("</li>");
+          format!("<li>{item}</li>")
 
-        ul_html.push_str(list_item.as_str());
-        list_item.clear();
-    }
-    ul_html.push_str("</ul>");
+       }).collect::<String>();
 
-    ul_html
+   format!("<ul>{list_item}</ul>")
 }
 
 #[cfg(test)]
@@ -30,7 +25,7 @@ mod tests {
     #[test]
     fn generates_unordered_list() {
         assert_eq!(
-            generate_unordered_list(unordered_list {
+            generate_unordered_list(UnorderedList {
                 list_items: vec![
                     "hi".to_string(),
                     "hello".to_string(),
