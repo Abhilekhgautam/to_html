70004d200c269beca16025b45b8cef18b2bd028f
abhilekh <abhilekhgautam119@gmail.com>
2022-10-17 01:07:29 +0545

    constructor for image added

--- src/image.rs
+++ src/image.rs
@@ -1,27 +1,76 @@
 ///This represents img tag in html.
 /// The height and width of the Image defaults to 500.
 #[derive(PartialEq, Debug)]
-struct Image {
-    src: String,
-    alt: Option<String>,
-    height: Option<String>,
-    width: Option<String>,
+struct Image<'a> {
+    src: &'a str,
+    alt: Option<&'a str>,
+    height: Option<&'a str>,
+    width: Option<&'a str>,
+}
+
+impl<'a> Image<'a> {
+    fn new(s: &'a str) -> Self {
+        let stripped_img_front = match s.strip_prefix("img(") {
+            Some(str) => str,
+            None => "",
+        };
+        //stripped_img now contains only the parameters to img
+        let stripped_img = match stripped_img_front.strip_suffix(")") {
+            Some(str) => str,
+            None => "",
+        };
+        let param: Vec<_> = stripped_img.split(",").collect();
+
+        let param_nos = param.len();
+
+        if param_nos > 4 && param_nos < 1 {
+            panic!("Invalid img parameters");
+        } else if param_nos == 4 {
+            Image {
+                src: param[0],
+                alt: Some(param[1]),
+                height: Some(param[2]),
+                width: Some(param[3]),
+            }
+        } else if param_nos == 3 {
+            Image {
+                src: param[0],
+                alt: Some(param[1]),
+                height: Some(param[2]),
+                width: None,
+            }
+        } else if param_nos == 2 {
+            Image {
+                src: param[0],
+                alt: Some(param[1]),
+                height: None,
+                width: None,
+            }
+        } else {
+            Image {
+                src: param[0],
+                alt: None,
+                height: None,
+                width: None,
+            }
+        }
+    }
 }
 
 fn generate_image(img: Image) -> String {
     let alt = match img.alt {
         Some(str) => str,
-        None => "".to_string(),
+        None => "",
     };
 
     let height = match img.height {
         Some(str) => str,
-        None => "500".to_string(),
+        None => "500",
     };
 
     let width = match img.width {
         Some(str) => str,
-        None => "500".to_string(),
+        None => "500",
     };
 
     let src = img.src;
@@ -38,8 +87,8 @@ mod tests {
     fn generates_img() {
         assert_eq!(generate_image(Image {
 
-       src    : String::from("https://abhi-lekhgautam.web.app/pp"),
-       alt    : Some("Abhilekh Gautam's profile picture".to_string()),
+       src    : "https://abhi-lekhgautam.web.app/pp",
+       alt    : Some("Abhilekh Gautam's profile picture"),
        height : None,
        width  : None,
      }), r#"<img src = "https://abhi-lekhgautam.web.app/pp" alt = "Abhilekh Gautam's profile picture" height = "500" width = "500"/>"#.to_string());
